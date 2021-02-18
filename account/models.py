4d3c34c3cb2658fbfdee896fc877119130dac503
maysu1914 <aysum1914@gmail.com>
2021-02-19 00:36:20 +0300

    Update .gitignore; Add fixtures to account app; Update models; Override clean function in transaction model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,6 @@
+import copy
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
@@ -14,28 +17,71 @@ class Customer(models.Model):
 
 
 class Account(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
-    deposit = models.PositiveIntegerField(_("Deposit"), default=500)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    deposit = models.IntegerField(_("Deposit"), default=500)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
-        return str(self.customer.name) if self.customer else _("Customer not found")
+        show = [str(self.customer.name), _("Deposit") + ': ' + str(self.deposit)]
+        return ' | '.join(show)
 
 
 class Transaction(models.Model):
-    account_from = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name='account_from')
-    account_to = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name='account_to')
+    account_from = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, related_name='account_from')
+    account_to = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, related_name='account_to')
     amount = models.PositiveIntegerField(_("Amount"))
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
+    def __init__(self, *args, **kwargs):
+        super(Transaction, self).__init__(*args, **kwargs)
+        self.old_instance = copy.deepcopy(self)
+
     def save(self, *args, **kwargs):
-        account_from = self.account_from
-        account_to = self.account_to
-        account_from.deposit -= self.amount
-        account_to.deposit += self.amount
+        if not self._state.adding:
+            if self.account_from != self.old_instance.account_from:
+                self.old_instance.account_from.save()
+            if self.account_to != self.old_instance.account_to:
+                self.old_instance.account_to.save()
+        self.account_from.save()
+        self.account_to.save()
         super(Transaction, self).save(*args, **kwargs)
 
+    def clean(self):
+        # Don't allow transactions to the same account
+        if self.account_from == self.account_to:
+            raise ValidationError(_('Transaction may not be done to the same account.'))
+        # Don't allow transactions more than the deposit
+        else:
+            if not self._state.adding:
+                # if accounts changes
+                if self.account_from != self.old_instance.account_from or self.account_to != self.old_instance.account_to:
+                    if self.old_instance.amount > self.old_instance.account_to.deposit:
+                        raise ValidationError(
+                            {'amount': _(
+                                'Transaction may not be undone due to insufficient balance of ') + str(
+                                self.old_instance.account_to)})
+                    self.old_instance.account_from.deposit += self.old_instance.amount
+                    self.old_instance.account_to.deposit -= self.old_instance.amount
+                    if self.account_from == self.old_instance.account_from:
+                        self.account_from.deposit = self.old_instance.account_from.deposit
+                    if self.account_to == self.old_instance.account_to:
+                        self.account_to.deposit = self.old_instance.account_to.deposit
+                else:
+                    if self.old_instance.amount > self.account_to.deposit + self.amount:
+                        raise ValidationError(
+                            {'amount': _(
+                                'Transaction may not be undone due to insufficient balance of ') + str(
+                                self.account_to)})
+                    self.account_from.deposit += self.old_instance.amount
+                    self.account_to.deposit -= self.old_instance.amount
+            if self.amount > self.account_from.deposit:
+                raise ValidationError(
+                    {'amount': _('Transaction may not more than the deposit of the customer.')})
+            self.account_from.deposit -= self.amount
+            self.account_to.deposit += self.amount
+
     def __str__(self):
-        return str(self.account_from) + ' -> ' + str(self.account_to) + ' | ' + str(self.amount)
+        return str(self.account_from.customer.name) + ' -> ' + str(self.account_to.customer.name) + ' | ' + str(
+            self.amount)
